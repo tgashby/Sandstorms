bd8c9498c3a810331a8fc7593cf4a852849cd250
Tag Ashby <tgashby@calpoly.edu>
2011-08-11 11:58:47 -0700

    Character pseudocode complete

--- include/Character.h
+++ include/Character.h
@@ -1,14 +1,42 @@
 #pragma once
 
+#include <ProjIncludes.h>
+#include <Texture.h>
+#include <Animation.h>
+
 namespace Sandstorms
 {
 	class Character
 	{
 	public:
-		Character(int health);
-		virtual ~Character(void);
+		Character(std::string texName, int lvl, int xPos = 0, int yPos = 0);
+		virtual ~Character();
 
-		virtual void 
+		virtual void moveToward(Character& who) = 0;
+		virtual void attack(Character& who) = 0;
+		
+		void takeDamage(int damage);
+		int getHealth();
+		SDL_Point getPosition();
+		SDL_Rect getBounds();
+
+	protected:
+		virtual void initAttributes(int level) = 0;
+		void setX(int xVal);
+		void setY(int yVal);
+		void setPosition(SDL_Point newCoords);
+
+		TGA::Texture texture;
+		TGA::Animation animation;
+
+		int health, level, speed;
+		SDL_Point coords;
+		SDL_Rect bounds;
+
+		float xVel, yVel; 
+		float xAccel, yAccel;
+		
+		//std::vector<Attack> attacks;
 	};
 }
 
--- src/Character.cpp
+++ src/Character.cpp
@@ -2,11 +2,52 @@
 
 namespace Sandstorms
 {
-	Character::Character(void)
+	Character::Character(std::string texName, int lvl, int xPos /*= 0*/, int yPos /*= 0*/)
+	 : level(lvl)
 	{
+		// Initialize the texture to loadImage(texName.c_str())
+		// Set coords x
+		// Set coords y
 	}
 
-	Character::~Character(void)
+	Character::~Character() {}
+
+	void Character::takeDamage(int damage)
 	{
+		// Remove damage from health
+
+		// If the health is <= 0
+			// Delete the animation
+			// Delete the texture
+	}
+
+	int Character::getHealth()
+	{
+		// RETURN current health
+	}
+
+	SDL_Point Character::getPosition()
+	{
+		// RETURN the position
+	}
+
+	SDL_Rect Character::getBounds()
+	{
+		// RETURN the bounds
+	}
+
+	void Character::setX( int xVal )
+	{
+		// Set the coords.x
+	}
+
+	void Character::setY( int yVal )
+	{
+		// Set the coords.y
+	}
+
+	void Character::setPosition( SDL_Point newCoords )
+	{
+		// Set the coords
 	}
 }
